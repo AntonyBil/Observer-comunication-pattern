8964d9e281440a95dea515f69f0be41e7e360375
AntonyBil <beletskiy.ant@gmail.com>
2023-04-18 19:51:16 +0300

    Initial commit

---
+++ 7_Observer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  7_Observer
+//
+//  Created by apple on 18.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
